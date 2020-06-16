b75634828a6091cb57a304936b29c69e0ee601d4
Maria <itimasha@gmail.com>
2020-06-17 01:54:11 +0300

    make main component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+
+const PromoDefault = {
+  TITLE: `Star Wars`,
+  GENRE: `Drama`,
+  RELEASE: 2017,
+};
+
+ReactDOM.render(
+  <App
+    promoTitle= {PromoDefault.TITLE}
+    promoGenre= {PromoDefault.GENRE}
+    promoRelease= {PromoDefault.RELEASE}
+  />,
+  document.querySelector(`#root`)
+);
 
